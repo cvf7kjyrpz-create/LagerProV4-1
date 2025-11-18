c718c841fd9b431d76966aba6c1f620e832d33b0
cvf7kjyrpz-create <cvf7kjyrpz@privaterelay.appleid.com>
2025-11-18 14:54:55 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,36 @@
+const CACHE = "lager-pro-v4-cache";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then(
+      (cached) =>
+        cached ||
+        fetch(event.request).catch(() =>
+          caches.match("./index.html")
+        )
+    )
+  );
+});
